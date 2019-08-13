9c3137575335a5fd9a03d2866e9afb97b277358f
Gideon <Gideon.tobing@Hotmail.com>
2019-08-13 11:20:36 +0700

    GridLayoutManager and SpanSizeLookup

--- app/src/main/java/com/don/recyclerview/MainActivity.kt
+++ app/src/main/java/com/don/recyclerview/MainActivity.kt
@@ -2,22 +2,28 @@ package com.don.recyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true)
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        val spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position % 4 == 0) {
+                    3
+                } else {
+                    1
+                }
+            }
+        }
+        gridLayoutManager.spanSizeLookup = spanSizeLookup
+        recycler_view.layoutManager = gridLayoutManager
         recycler_view.adapter = SimpleRVAdapter()
-        recycler_view.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL))
-
     }
 }
